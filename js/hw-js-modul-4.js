669bb0411432eaab7aae15e313b567c44be4e913
Oleksii Bohatyrenko <steenbras31@gmail.com>
2021-01-31 02:07:25 +0200

    Добавляет решение 2-6 задач 4-го модуля

--- js/hw-js-modul-4.js
+++ js/hw-js-modul-4.js
@@ -6,3 +6,126 @@
 // const pointer = makePizza;
 
 /** 2/10 */
+
+// function deliverPizza(pizzaName) {
+//   return `Доставляем пиццу ${pizzaName}.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Пицца ${pizzaName} готовится, ожидайте...`;
+// }
+
+// // Пиши код ниже этой строки
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+
+// makeMessage("Роял гранд", makePizza);
+// makeMessage("Ультрасыр", deliverPizza);
+
+//** Пример к задаче № 2 */
+// function greet(name) {
+//   console.log(`Добро пожаловать ${name}.`);
+// }
+
+// function registerGuest(name, callback) {
+//   console.log(`Регистрируем гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest("Jon", greet);
+
+/** 3/10 */
+// function makePizza(pizzaName, callback) {
+//   console.log(`Пицца ${pizzaName} готовится, ожидайте...`);
+//   callback(pizzaName);
+// }
+
+// makePizza("Роял гранд", function deliverPizza(pizzaName) {
+//   console.log(`Доставляем пиццу ${pizzaName}.`);
+// });
+// // Пиши код ниже этой строки
+// makePizza("Ультрасыр", function eatPizza(pizzaName) {
+//   console.log(`Едим пиццу ${pizzaName}.`);
+// });
+
+/** 4/10 */
+// const pizzaPalace = {
+//   pizzas: ["Ультрасыр", "Аль Копчино", "Четыре нарезона"],
+//   order(pizzaName, onSuccess, onError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       return onSuccess(pizzaName);
+//     }
+//     return onError(`В ассортименте нет пиццы с названием ${pizzaName}.`);
+//   },
+// };
+// // Пиши код выше этой строки
+
+// // Колбэк для onSuccess
+// function makePizza(pizzaName) {
+//   return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+// }
+
+// // Колбэк для onError
+// function onOrderError(error) {
+//   return `Ошибка! ${error}`;
+// }
+
+// // Вызовы метода с колбэками
+// console.log(pizzaPalace.order("Аль Копчино", makePizza, onOrderError));
+// console.log(pizzaPalace.order("Четыре нарезона", makePizza, onOrderError));
+// console.log(pizzaPalace.order("Биг майк", makePizza, onOrderError));
+// console.log(pizzaPalace.order("Венская", makePizza, onOrderError));
+
+/** 5/10 */
+// const pizzaPalace = {
+//   pizzas: ["Ультрасыр", "Аль Копчино", "Четыре нарезона"],
+//   // Пиши код ниже этой строки
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
+
+//     if (!isPizzaAvailable) {
+//       return `В ассортименте нет пиццы с названием «${pizzaName}».`;
+//     }
+
+//     return `Заказ принят, готовим пиццу «${pizzaName}».`;
+//   },
+//   // Пиши код выше этой строки
+// };
+
+/** 6/10 */
+// const customer = {
+//   username: 'Mango',
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ['Burger', 'Pizza', 'Salad'],
+//   // Пиши код ниже этой строки
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+//   // Пиши код выше этой строки
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, 'Steak');
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ['Burger', 'Pizza', 'Salad', 'Steak']
+
+/** 7/10 */
--- js/practik.js
+++ js/practik.js
@@ -1,18 +1,10 @@
-// Давайте произведём 5 операций с массивом.
+const a = function (name) {
+  console.log(name);
+};
 
-// Создайте массив styles с элементами «Джаз» и «Блюз».
-// Добавьте «Рок-н-ролл» в конец.
-// Замените значение в середине на «Классика». Ваш код для поиска значения в середине должен работать для массивов с любой длиной.
-// Удалите первый элемент массива и покажите его.
-// Вставьте «Рэп» и «Регги» в начало массива.
+const b = function (newName) {
+  const c = "Jon";
+  newName(c);
+};
 
-const styles = ['Джаз', 'Блюз', 'fdasf'];
-styles.push('Рок-н-ролл');
-console.log(styles);
-styles.splice(Math.round(styles.length / 2)-1, 1,'Классика');
-console.log(styles);
-console.log(styles.shift());
-styles.unshift('Рэп', 'Регги');
-console.log(styles);
-
-
+b(a);
