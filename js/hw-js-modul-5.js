cb15b3c380d511bb7ed87d0934a3fbe744f77172
Oleksii Bohatyrenko <steenbras31@gmail.com>
2021-02-02 00:00:17 +0200

    Добавляет решение 1-5 задач 5 модуля

---
+++ js/hw-js-modul-5.js
@@ -0,0 +1,53 @@
+/** 1/19 */
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+
+// const child = Object.create(parent);
+
+// child.name = "Jason";
+// child.age = 27;
+
+/** 2/19 */
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+/** 3/19 */
+// function Car(brand, model, price) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+
+// new Car("Audi", "Q3", 36000);
+// new Car("BMW", "X5", 58900);
+// new Car("Nissan", "Murano", 31700);
+
+/** 4/19 */
+// function Car({ brand, model, price }) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+
+// new Car("Audi", "Q3", 36000);
+// new Car("BMW", "X5", 58900);
+// new Car("Nissan", "Murano", 31700);
+
+/** 5/19 */
--- js/practik.js
+++ js/practik.js
@@ -1,13 +1,22 @@
-function abbrevName(name) {
-  let newName = name
-    .split(" ")
-    .map(function (item) {
-      return item[0];
-    })
-    .join(".");
-  console.log(newName);
+// function abbrevName(name) {
+//   let newName = name
+//     .split(" ")
+//     .map(function (item) {
+//       return item[0];
+//     })
+//     .join(".");
+//   console.log(newName);
 
-  return newName.toUpperCase();
+//   return newName.toUpperCase();
+// }
+
+// console.log(abbrevName("Jon Abram"));
+
+function opposite(number) {
+  if (number < 0) {
+    return number * -1;
+  }
+  return number * -1;
 }
 
-console.log(abbrevName("Jon Abram"));
+console.log(opposite(-12525220.3325));
