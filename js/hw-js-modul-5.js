97763b4d89f1de8d6d65674b3e99ade96fa588e6
Oleksii Bohatyrenko <steenbras31@gmail.com>
2021-02-03 01:04:25 +0200

    Добавляет решение 5-6  задач 5-го модуля

--- js/hw-js-modul-5.js
+++ js/hw-js-modul-5.js
@@ -51,3 +51,57 @@
 // new Car("Nissan", "Murano", 31700);
 
 /** 5/19 */
+// function Car({ brand, model, price }) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+
+// Car.prototype.getPrice = function () {
+//   return this.price;
+// };
+
+// Car.prototype.changePrice = function (newPrice) {
+//   this.price = newPrice;
+// };
+
+/** 6/19 */
+function Storage(items) {
+  this.items = items;
+}
+
+Storage.prototype.getItems = function () {
+  return this.items;
+};
+Storage.prototype.addItem = function (newItem) {
+  this.items.push(newItem);
+};
+Storage.prototype.removeItem = function (item) {
+  return this.items.splice(this.items.indexOf(item), 1);
+};
+// Пиши код выше этой строки
+const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
+console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+storage.addItem("Дроид");
+console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+storage.removeItem("Пролонгер");
+console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+
+/** 7/19 */
+function StringBuilder(baseValue) {
+  this.value = baseValue;
+}
+
+StringBuilder.prototype.getValue = function () {
+  return this.basevalue;
+};
+StringBuilder.prototype.padEnd = function (str) {
+  this.value.push(str);
+};
+StringBuilder.prototype.padStart = function (str) {
+  this.value.unshift(str);
+};
+/**ДОДЕЛАТЬ */
+//StringBuilder.prototype.padBoth = function (str) {
+//  this.value.unshift(str);
+//};
