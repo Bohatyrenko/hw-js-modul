52ec7b6cd24a326d47fc93fcfd4ccc42e863bc0d
Oleksii Bohatyrenko <steenbras31@gmail.com>
2021-02-05 01:29:24 +0200

    Добавляет решение 7-10 задач 5-го модуля

--- js/hw-js-modul-5.js
+++ js/hw-js-modul-5.js
@@ -66,42 +66,86 @@
 // };
 
 /** 6/19 */
-function Storage(items) {
-  this.items = items;
-}
+// function Storage(items) {
+//   this.items = items;
+// }
 
-Storage.prototype.getItems = function () {
-  return this.items;
-};
-Storage.prototype.addItem = function (newItem) {
-  this.items.push(newItem);
-};
-Storage.prototype.removeItem = function (item) {
-  return this.items.splice(this.items.indexOf(item), 1);
-};
-// Пиши код выше этой строки
-const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
-console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
-storage.addItem("Дроид");
-console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
-storage.removeItem("Пролонгер");
-console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+// Storage.prototype.getItems = function () {
+//   return this.items;
+// };
+// Storage.prototype.addItem = function (newItem) {
+//   this.items.push(newItem);
+// };
+// Storage.prototype.removeItem = function (item) {
+//   return this.items.splice(this.items.indexOf(item), 1);
+// };
+// // Пиши код выше этой строки
+// const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem("Дроид");
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem("Пролонгер");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
 
 /** 7/19 */
-function StringBuilder(baseValue) {
-  this.value = baseValue;
-}
+// function StringBuilder(baseValue) {
+//   this.value = baseValue;
+// }
 
-StringBuilder.prototype.getValue = function () {
-  return this.basevalue;
-};
-StringBuilder.prototype.padEnd = function (str) {
-  this.value.push(str);
-};
-StringBuilder.prototype.padStart = function (str) {
-  this.value.unshift(str);
-};
-/**ДОДЕЛАТЬ */
-//StringBuilder.prototype.padBoth = function (str) {
-//  this.value.unshift(str);
-//};
+// StringBuilder.prototype.getValue = function () {
+//   return this.value;
+// };
+
+// StringBuilder.prototype.padEnd = function (str) {
+//   this.value += str;
+// };
+// StringBuilder.prototype.padStart = function (str) {
+//   this.value = str + this.value;
+// };
+
+// StringBuilder.prototype.padBoth = function (str) {
+//   this.value = str + this.value + str;
+// };
+
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // '.'
+// builder.padStart("^");
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd("^");
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth("=");
+// console.log(builder.getValue()); // '=^.^='
+
+/** 8/19 */
+// class Car { };
+
+/** 9/19 */
+// class Car {
+//   brand;
+//   model;
+//   price;
+
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+
+/** 10/19 */
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+
+//   getPrice() {
+//     return this.price;
+//   }
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+// }
+
+/** 11/19 */
